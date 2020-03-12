da717908714cd3dcce07f4eb97aa970e22935249
Kimani Ndegwa <kimani@pave.solutions>
2020-03-12 23:14:26 +0300

    Addresses story 3

--- client/helpers.js
+++ client/helpers.js
@@ -2,35 +2,36 @@ const getUnitCharge = (category, durationInDays) => {
     let baseCharge;
     let excessCharge;
     let totalCharge;
+
     if(category === 'regular'){
         if(durationInDays > 2){
         excessCharge = 1.5;
         baseCharge = 2;
-        totalCharge = baseCharge + (excessCharge * durationInDays - 2);
+        totalCharge = baseCharge + (excessCharge * (durationInDays - 2));
+        } else if (durationInDays === 2){
+            totalCharge = durationInDays * 1.5;
         } else {
-            totalCharge = 2;
+            totalCharge = baseCharge;
         }
     }
 
     if(category === 'novel'){
         baseCharge = 4.5;
-        if(durationInDays > 3){
-            totalCharge = 3 * durationInDays;
-        } else if (durationInDays < 3){
-            totalCharge = minimumCharge;
-        };
+        if(durationInDays >= 3){
+            excessCharge = 1.5;
+            totalCharge = baseCharge + (excessCharge * (durationInDays - 3));
+        } else if (durationInDays === 3){
+            totalCharge = durationInDays * 1.5
+        } else {
+            totalCharge = baseCharge;
+        }
     };
 
     if(category === "fiction"){
         totalCharge = durationInDays * 3
     };
 
-    console.log(totalCharge, "total chareg")
-
-    return {
-        value: totalCharge,
-        displayValue: `$${totalCharge}`
-    };
+    return totalCharge;
 
 };
 
@@ -40,13 +41,12 @@ export const calculateTotalCharge = (selectedBooks) => {
         displayValue: '$0'
     };
 
-    const charges = selectedBooks.map(book=> book.durationInDays * getUnitCharge(book.category, book.durationInDays));
+    const charges = selectedBooks.map(book=> getUnitCharge(book.category, book.durationInDays));
     const reducer = (acc, curr) => {
         return acc + curr
     };
 
     const totalCharge = charges.reduce(reducer, 0);
-    console.log(selectedBooks, "select")
     return {
         value: totalCharge,
         displayValue: `$${totalCharge}`
